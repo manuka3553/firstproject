87490e0d98d947ec45e39b16706482d693822d2b
manuka3553 <manukrishna.clt007@gmail.com>
2021-01-01 15:52:50 +0530

    Hirocu update

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(t^fo1p@zij)pjp%z9swx)xu%wub#3ty5-ka9jhi)x))-%zsk-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
